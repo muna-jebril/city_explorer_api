f12c118dd742296fa672b7a47f75a1c21a065d8b
muna-jebril <muna.jebril1995@hotmail.com>
2020-04-05 16:40:46 +0300

    finshe

--- server.js
+++ server.js
@@ -11,4 +11,25 @@ server.listen(PORT,() =>{
 server.get('/',(request , response)=>{
 response.status(200).send('it works');
 })
- 
+  server.get('/location',(req,res)=>{
+      const data = require("./data/geo.json");
+      const city = req.query.city;
+      const locationData= new Location (city,data);
+      res.send(locationData);
+
+      
+  })
+
+  function Location (city, data){
+    this.search_query = city;
+    this.formatted_query = data[0].display_name;
+    this.latitude = data[0].lat;
+    this.longitude = data[0].lng;
+  }
+  server.use('*',(req,res)=>{
+      res.status(404).send('not fount');
+
+  });
+  server.use((error,req,res)=>{
+      res.status(500).send(error);
+  })
